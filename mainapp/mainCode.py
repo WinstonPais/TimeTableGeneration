64b6f2ab17b4bd2e4a099309db3251f4172fa268
WinstonPais <51374812+WinstonPais@users.noreply.github.com>
2020-03-14 22:47:54 +0530

    Added FirstYear Input With Faculty TimeTables

--- mainapp/mainCode.py
+++ mainapp/mainCode.py
@@ -31,10 +31,18 @@ conn.autocommit = True
 #         retStr="subject id:"+str(self.subid)+"\nDay:"+str(self.day)+"\nHour:"+str(self.hour)+"\nLecturer Name:"+str(self.lecName)
 #         return retStr
 
+'''
+delete from subjects_ctt;
+delete from subjects_stt;
+delete from class_time_table;
+delete from staff_time_table;
+SELECT SETVAL((SELECT pg_get_serial_sequence('class_time_table', 'ctt_id')), 1, false);
+SELECT SETVAL((SELECT pg_get_serial_sequence('staff_time_table', 'stt_id')), 1, false);
+'''
 def resetCounters():
     cur=conn.cursor()
     cur.execute("SELECT SETVAL((SELECT pg_get_serial_sequence('class_time_table', 'ctt_id')), 1, false)")
-
+    cur.execute("SELECT SETVAL((SELECT pg_get_serial_sequence('staff_time_table', 'stt_id')), 1, false)")
     cur.close()
 
 def resetTables():
@@ -49,10 +57,10 @@ def resetTables():
     cur.close()
     resetCounters()
 
-def addinputNoSubject(staff_id,day,period,class_id):
-    cur=conn.cursor()
-    cur.execute("insert into staff_time_table (staff_id,day,period,class_id) values ('"+str(staff_id)+"','"+str(day)+"',"+period+",'"+str(class_id)+"')")
-    cur.close()
+# def addinputNoSubject(staff_id,day,period,class_id):
+#     cur=conn.cursor()
+#     cur.execute("insert into staff_time_table (staff_id,day,period,class_id) values ('"+str(staff_id)+"','"+str(day)+"',"+period+",'"+str(class_id)+"')")
+#     cur.close()
 
 def addInputNoStaff(sub_id,day,period,class_id):
     cur=conn.cursor()
@@ -71,12 +79,12 @@ def addInputWithStaffAndSubject(staff_id,sub_id,day,period,class_id):
     cur.execute("insert into class_time_table (staff_id,sub_id,day,period,class_id) values ('"+str(staff_id)+"','"+str(sub_id)+"','"+str(day)+"',"+str(period)+",'"+str(class_id)+"') ")
     cur.execute("select ctt_id from class_time_table where staff_id='"+str(staff_id)+"' and sub_id='"+str(sub_id)+"' and day='"+str(day)+"' and period="+str(period)+" and class_id='"+str(class_id)+"'")
     ctt_id=cur.fetchall()[0][0]
-    cur.execute("insert into subjects_ctt values("+str(sub_id)+"','"+str(ctt_id)+"')")
+    cur.execute("insert into subjects_ctt values('"+str(sub_id)+"',"+str(ctt_id)+")")
     cur.execute("insert into staff_time_table (staff_id,sub_id,day,period,class_id) values ('"+str(staff_id)+"','"+str(sub_id)+"','"+str(day)+"',"+str(period)+",'"+str(class_id)+"') ")
     cur.execute("select stt_id from staff_time_table where staff_id='"+str(staff_id)+"' and sub_id='"+str(sub_id)+"' and day='"+str(day)+"' and period="+str(period)+" and class_id='"+str(class_id)+"'")
     stt_id=cur.fetchall()[0][0]
-    cur.execute("insert into subjects_stt values("+str(sub_id)+"','"+str(stt_id)+"')")
-    cur.execute("insert into staffdetails_ctt values("+str(staff_id)+"','"+str(ctt_id)+"')")
+    cur.execute("insert into subjects_stt values('"+str(sub_id)+"',"+str(stt_id)+")")
+    cur.execute("insert into staffdetails_ctt values('"+str(staff_id)+"',"+str(ctt_id)+")")
 
     cur.close()
 
@@ -110,7 +118,59 @@ def getClassTimeTable(class_id):
     # print(retString)
     return retList
 
+def getStaffTimeTable(staff_id):
+    retList=[]
+    # retString=str(class_id)+"\n"
+    # print(class_id)
+    cur=conn.cursor()
+    # retString+="   \t1    \t2    \t3    \t4    \t5    \t6    \t7"+"\n"
+    # print("   \t1    \t2    \t3    \t4    \t5    \t6    \t7")
+    for day in daysList:
+        dayList=[]
+        # retString+=day+"\t"
+        # print(day+"\t",end="")
+        for period in range(1,8):
+            cur.execute("select sub_id from staff_time_table where staff_id='"+staff_id+"' and day='"+day+"' and period="+str(period))
+
+            try:                                        #if cur is empty, then fetching will throw an error
+                # print(cur.fetchone()[0]+"\t",end="")
+                # retString+=cur.fetchone()[0]+"\t"
+                dayList.append(cur.fetchone()[0])
+            except:
+                # retString+="NULL\t"
+                dayList.append("NULL")
+                # print("NULL\t",end="")
+        # print()
+        # retString+="\n"
+        retList.append(dayList)
+    # print("-----------------------------------------------------------")
+    cur.close()
+    # print(retString)
+    return retList
+
+
 def addinput():
+    # addInputWithStaffAndSubject(staff_id,sub_id,day,period,class_id)
+    addInputWithStaffAndSubject("KKM","CPS","TUE",4,"F")
+    addInputWithStaffAndSubject("KKM","CPS","WED",2,"F")
+    addInputWithStaffAndSubject("KKM","CPS","THU",1,"F")
+    addInputWithStaffAndSubject("KKM","CPS","FRI",5,"F")
+
+    addInputWithStaffAndSubject("SPS","CPS","MON",5,"G")
+    addInputWithStaffAndSubject("SPS","CPS","WED",4,"G")
+    addInputWithStaffAndSubject("SPS","CPS","THU",2,"G")
+    addInputWithStaffAndSubject("SPS","CPS","FRI",1,"G")
+
+    addInputWithStaffAndSubject("TYN","CPS","MON",2,"H")
+    addInputWithStaffAndSubject("TYN","CPS","TUE",5,"H")
+    addInputWithStaffAndSubject("TYN","CPS","WED",1,"H")
+    addInputWithStaffAndSubject("TYN","CPS","FRI",4,"H")
+
+    addInputWithStaffAndSubject("SVG","CPS","MON",3,"I")
+    addInputWithStaffAndSubject("SVG","CPS","TUE",2,"I")
+    addInputWithStaffAndSubject("SVG","CPS","WED",1,"I")
+    addInputWithStaffAndSubject("SVG","CPS","THU",5,"I")
+
     addInputNoStaff("MAT-III", "MON", 1, "3A")
     addInputNoStaff("KAN", "MON", 6, "3A")
     addInputNoStaff("MAT-III", "TUE", 4, "3A")
@@ -130,11 +190,17 @@ def addinput():
 
 def getTT():
     addinput()
-    classList=["3A","3B"]
+    classList=["3A","3B","F","G","H","I"]
     TTList=[]
     for classname in classList:
         TTList.append(getClassTimeTable(classname))
     # TTList.append(getClassTimeTable("3B"))
 
+    facultyList=["KKM","SPS","TYN","SVG"]
+    FTTList=[]
+    for staff_id in facultyList:
+        FTTList.append(getStaffTimeTable(staff_id))
+
     resetTables()
-    return TTList,classList
+    # print(FTTList)
+    return TTList,classList,FTTList,facultyList
--- mainapp/views.py
+++ mainapp/views.py
@@ -5,8 +5,9 @@ def index(req):
     return render(req,'index.html')
 
 def timetabledisp(req):
-    a,classList=getTT()
-    length=range(len(a))
+    TTList,classList,facultyList,FTTList=getTT()
+    classLength=range(len(classList))
+    facultyLength=range(len(facultyList))
     range5=range(5)
     range7=range(7)
     # classList=["3A","3B"]
@@ -14,5 +15,5 @@ def timetabledisp(req):
     # print(a)
     # print(len(a),length,range5,range7)
 
-    myDict={'timetables':a,'cl':classList,'dl':daysList,'length':length,'range5':range5,'range7':range7}
+    myDict={'timetables':TTList,'cl':classList,'dl':daysList,'noOfClasses':classLength,'range5':range5,'range7':range7,'ftimetables':facultyList,'fl':FTTList,'facultylen':facultyLength}
     return render(req,'timetable.html',context=myDict)
