c9043de37bfbb9e71d81b12a5cf44f6d23351856
Wilton <wiltondsouza100@gmail.com>
2020-07-09 17:00:56 +0530

    data from input page to database

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -4,5 +4,6 @@ from mainapp import views
 app_name = 'mainapp'
 urlpatterns = [
     path('', views.index, name='index'),
-    path('input/', views.input, name='inputPage'),
+    path('input/',views.input,name='inputPage'),
+
 ]
